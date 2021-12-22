5f10ebd7fbd69c1bcbb3dfc28c221568bb8d1cc2
GulzarJS <gulzarsafar@outlook.com>
2021-12-22 12:03:46 +0400

    UI is ready

--- src/audio.py
+++ src/audio.py
@@ -1,18 +1,41 @@
 from playsound import playsound
 import sounddevice as sd
 import soundfile as sf
+import pygame as pg
 from pydub import AudioSegment
 import scipy.io.wavfile as wf
 import os
 
 
 class Audio:
+
+    def __init__(self):
+        self.pause = True
+        self.filename = "hello.mp3"
+
+    def set_filename(self, filename):
+        self.filename = filename
+
     def read_audio_file(self, filename):
         sound, samplerate = sf.read(filename)
         return sound, samplerate
 
     def play_audio_file(self, filename):
         playsound("audio_files/" + filename)
+
+    def play_audio_file_new(self):
+        pg.init()
+        pg.mixer.music.load("audio_files/" + self.filename)
+        pg.mixer.music.play(1, 0.0)
+        self.pause = False
+
+    def pause_audio_file(self):
+        if self.pause:
+            pg.mixer.music.unpause()
+            self.pause = False
+        else:
+            pg.mixer.music.pause()
+            self.pause = True
 
 
     def create_audio_file(self, filename):
--- src/ui.py
+++ src/ui.py
@@ -1,10 +1,11 @@
 import os
 import tkinter as tk
+import random
 
 from src.audio import Audio
 
-HEIGHT = 200
-WIDTH = 400
+HEIGHT = 300
+WIDTH = 500
 
 class UI:
 
@@ -13,10 +14,21 @@ class UI:
         self.window = tk.Tk()  # to create UI window
         self.window.title("PlaySound World")
         self.create_canvas()  # to create canvas of window
-        self.canvas.grid(row=0, column=0, columnspan=3)
+        self.canvas.grid(row=0, column=0, columnspan=6)
+        self.audio_wire()
         self.create_buttons()
         self.info = tk.Label(self.window, text="")  # information label of game
         self.info.grid(row=1, column=0, columnspan=3)
+
+
+    def audio_wire(self):
+        global HEIGHT
+        global WIDTH
+
+        i = 0
+        while ( i<WIDTH-10):
+            self.canvas.create_line(i,random.randint(20, HEIGHT//2),i,(random.randint(HEIGHT//2, HEIGHT-20))+5,fill = "white")
+            i = i + 8
 
 
 
@@ -25,18 +37,27 @@ class UI:
         global HEIGHT
         global WIDTH
 
-        self.canvas = tk.Canvas(self.window, bg="navy", width=WIDTH, height=HEIGHT)
+        self.canvas = tk.Canvas(self.window, bg="black", width=WIDTH, height=HEIGHT)
         self.canvas.pack()
 
     def create_buttons(self):
         button1 = tk.Button(self.window, text='Record', command= self.pop_up_window_record)
         button1.grid(row=4, column=0)
 
-        button2 = tk.Button(self.window, text='Play', command=self.pop_up_window_play)
+        button2 = tk.Button(self.window, text='Select file', command= self.pop_up_window_play)
         button2.grid(row=4, column=1)
 
-        button3 = tk.Button(self.window, text='Quit', command=self.window.destroy)
+        button3 = tk.Button(self.window, text='Play', command= self.audio.play_audio_file_new)
         button3.grid(row=4, column=2)
+
+        button4 = tk.Button(self.window, text='Pause', command = self.audio.pause_audio_file)
+        button4.grid(row=4, column=3)
+
+        button5 = tk.Button(self.window, text='Comment' )
+        button5.grid(row=4, column=4)
+
+        button6 = tk.Button(self.window, text='Quit', command=self.window.destroy)
+        button6.grid(row=4, column=5)
 
     def pop_up_window_record(self):
         global HEIGHT
@@ -73,7 +94,7 @@ class UI:
             var = tk.StringVar(win)
             menu = tk.OptionMenu(win,var, *file_list )
             menu.pack()
-            button = tk.Button(win, text = 'Select' , command = lambda:[self.audio.play_audio_file(var.get()), win.destroy()])
+            button = tk.Button(win, text = 'Select' , command = lambda:[self.audio.set_filename(var.get()), win.destroy()])
             button.pack()
 
 
