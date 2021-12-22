8eaa3d23bacca9415893f8b59106f52b81fdd664
GulzarJS <gulzarsafar@outlook.com>
2021-12-22 16:17:53 +0400

    Comment on Audio is ready

--- src/audio.py
+++ src/audio.py
@@ -5,29 +5,52 @@ import pygame as pg
 from pydub import AudioSegment
 import scipy.io.wavfile as wf
 import os
+from mutagen.mp3 import MP3
+import json
+
 
 
 class Audio:
 
     def __init__(self):
         self.pause = True
-        self.filename = "hello.mp3"
+        self.filename = ""
+        self.comment_dictionary = {}
 
     def set_filename(self, filename):
         self.filename = filename
+        print(self.get_duration())
 
-    def read_audio_file(self, filename):
-        sound, samplerate = sf.read(filename)
-        return sound, samplerate
+    # def read_audio_file(self):
+    #     sound, samplerate = sf.read("audio_files/" + self.filename)
+    #     return sound, samplerate
+
+    def get_current_position(self):
+        return int(pg.mixer.music.get_pos() / 1000)
+
+    def get_duration(self):
+        self.duration = (MP3("audio_files/" + self.filename)).info.length
+        return int(self.duration)
 
     def play_audio_file(self, filename):
         playsound("audio_files/" + filename)
+
+    def set_progress_bar(self, progress_bar):
+        self.progress_bar = progress_bar
+
+    def set_comment_label(self, comments):
+        self.comment_label = comments
+
+    def set_comment_box(self, entry):
+        self.comment_entry = entry
+
 
     def play_audio_file_new(self):
         pg.init()
         pg.mixer.music.load("audio_files/" + self.filename)
         pg.mixer.music.play(1, 0.0)
         self.pause = False
+        self.audio_progress()
 
     def pause_audio_file(self):
         if self.pause:
@@ -54,5 +77,52 @@ class Audio:
         sound.export("audio_files/" + filename + ".mp3", format='mp3')
         os.remove("audio_files/" + filename + ".wav")
 
-audio = Audio()
-audio.create_audio_file("hello")
+    def comment_on_audio_file(self):
+
+        # json_file = self.create_JSON_file(self.filename)
+
+
+
+        if self.current_position in self.comment_dictionary.keys():
+            self.comment_dictionary[self.current_position].append(self.comment_entry.get())
+        else:
+            self.comment_dictionary[self.current_position] = [self.comment_entry.get()]
+        # print(self.comment_dictionary)
+
+        with open("audio_comments/" + self.filename + ".json", "w") as json_file:
+            json.dump(self.comment_dictionary, json_file)
+
+        json_file.close()
+
+
+    def audio_progress(self):
+        self.current_position = int(pg.mixer.music.get_pos() / 1000)
+        self.progress_bar.config(text=str(self.current_position) + "|" + str(self.get_duration()))
+        with open("audio_comments/" + self.filename + ".json", "r") as json_file:
+            dict = json.load(json_file)
+            print(dict)
+            if str(self.current_position) in dict.keys():
+                self.comment_label.config(text=dict.get(str(self.current_position)))
+            else:
+                self.comment_label.config(text=" ")
+
+        json_file.close()
+
+        self.progress_bar.after(1000, self.audio_progress)
+
+    # def create_JSON_file(self, filename):
+    #
+    #     json_file = open("audio_comments/" + filename + ".json", "w")
+    #
+    #     return json_file
+
+
+#      TO DO
+#       Create normal popup windows(add positions)
+#       Correct recording(make duration 60 second and try to add stop button)
+#       Create text box with slider.
+#       Visualize audio Progress bar
+
+
+
+
--- src/ui.py
+++ src/ui.py
@@ -1,11 +1,13 @@
 import os
 import tkinter as tk
 import random
+import json
 
 from src.audio import Audio
 
 HEIGHT = 300
 WIDTH = 500
+
 
 class UI:
 
@@ -16,21 +18,26 @@ class UI:
         self.create_canvas()  # to create canvas of window
         self.canvas.grid(row=0, column=0, columnspan=6)
         self.audio_wire()
+        self.comments = tk.Label(self.window, text=" ")
+        self.comments.grid(row=3, column =0, columnspan=6)
+        self.audio.set_comment_label(self.comments)
+        self.create_progress_bar()
+        self.create_comment_box()
         self.create_buttons()
-        self.info = tk.Label(self.window, text="")  # information label of game
-        self.info.grid(row=1, column=0, columnspan=3)
 
+
+    def new_file(self):
+        self.comment_dictionary = {}
 
     def audio_wire(self):
         global HEIGHT
         global WIDTH
 
         i = 0
-        while ( i<WIDTH-10):
-            self.canvas.create_line(i,random.randint(20, HEIGHT//2),i,(random.randint(HEIGHT//2, HEIGHT-20))+5,fill = "white")
+        while (i < WIDTH - 10):
+            self.canvas.create_line(i, random.randint(20, HEIGHT // 2), i,
+                                    (random.randint(HEIGHT // 2, HEIGHT - 20)) + 5, fill="white")
             i = i + 8
-
-
 
     # Function to create new canvas of window
     def create_canvas(self):
@@ -40,24 +47,30 @@ class UI:
         self.canvas = tk.Canvas(self.window, bg="black", width=WIDTH, height=HEIGHT)
         self.canvas.pack()
 
+
     def create_buttons(self):
-        button1 = tk.Button(self.window, text='Record', command= self.pop_up_window_record)
+        button1 = tk.Button(self.window, text='Record', command=self.pop_up_window_record)
         button1.grid(row=4, column=0)
 
-        button2 = tk.Button(self.window, text='Select file', command= self.pop_up_window_play)
+        button2 = tk.Button(self.window, text='Select file', command=self.pop_up_window_play)
         button2.grid(row=4, column=1)
 
-        button3 = tk.Button(self.window, text='Play', command= self.audio.play_audio_file_new)
+        button3 = tk.Button(self.window, text='Play', command=self.audio.play_audio_file_new)
         button3.grid(row=4, column=2)
 
-        button4 = tk.Button(self.window, text='Pause', command = self.audio.pause_audio_file)
+        button4 = tk.Button(self.window, text='Pause', command=self.audio.pause_audio_file)
         button4.grid(row=4, column=3)
 
-        button5 = tk.Button(self.window, text='Comment' )
+        button5 = tk.Button(self.window, text='Comment', command=self.audio.comment_on_audio_file)
         button5.grid(row=4, column=4)
 
         button6 = tk.Button(self.window, text='Quit', command=self.window.destroy)
         button6.grid(row=4, column=5)
+
+    def create_comment_box(self):
+        self.entry = tk.Entry(self.window)
+        self.entry.grid(row=5, column=0, columnspan=6)
+        self.audio.set_comment_box(self.entry)
 
     def pop_up_window_record(self):
         global HEIGHT
@@ -72,30 +85,39 @@ class UI:
         tk.Label(win, text="Please, enter name of new audio file").place(x=50, y=50)
         e = tk.Entry(win)
         e.pack()
-        button = tk.Button(win, text = 'Start to record' , command = lambda:[self.audio.create_audio_file(e.get()), win.destroy()])
+        button = tk.Button(win, text='Start to record',
+                           command=lambda: [self.audio.create_audio_file(e.get()), win.destroy()])
         button.pack()
         win.mainloop()
 
-
     def pop_up_window_play(self):
-            global HEIGHT
-            global WIDTH
+        global HEIGHT
+        global WIDTH
 
-            win = tk.Toplevel(self.window)
-            win.geometry("400x200")
+        win = tk.Toplevel(self.window)
+        win.geometry("400x200")
 
-            tk.Label(win, text="Which audio file do you want to play?").place(x=50, y=50)
+        tk.Label(win, text="Which audio file do you want to play?").place(x=50, y=50)
 
-            directory = "./audio_files/"
-            file_list = os.listdir(directory)
-            print(file_list)
-            #
-            # options = tk.StringVar()
-            var = tk.StringVar(win)
-            menu = tk.OptionMenu(win,var, *file_list )
-            menu.pack()
-            button = tk.Button(win, text = 'Select' , command = lambda:[self.audio.set_filename(var.get()), win.destroy()])
-            button.pack()
+        directory = "./audio_files/"
+        file_list = os.listdir(directory)
+        print(file_list)
+        #
+        # options = tk.StringVar()
+        var = tk.StringVar(win)
+        menu = tk.OptionMenu(win, var, *file_list)
+        menu.pack()
+        button = tk.Button(win, text='Select',
+                           command=lambda: [self.audio.set_filename(var.get()), self.new_file, win.destroy()])
+        button.pack()
+
+    def create_progress_bar(self):
+        self.progress_bar = tk.Label(self.window, text=' ')
+        self.progress_bar.grid(row=1, column=0, columnspan=6)
+        self.audio.set_progress_bar(self.progress_bar)
+
+
+
 
 
 
